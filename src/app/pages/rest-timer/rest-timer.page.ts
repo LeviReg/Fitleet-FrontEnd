e8c200d079ec9636294bb450561ea75c8684da22
Daly12345 <35996640+Daly12345@users.noreply.github.com>
2019-11-24 21:14:00 +0000

    small css changes

--- src/app/pages/rest-timer/rest-timer.page.ts
+++ src/app/pages/rest-timer/rest-timer.page.ts
@@ -20,13 +20,23 @@ export class RestTimerPage implements OnInit {
   seconds: any = 30;
 
   //variables for overall time inside interpolation
+  elapsed: any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
 
+  totalTimer: any = false;
 
   startTimer() {
 
     //clears(stops it) the old timer every time the counter is clicked.
     if (this.timer) {
       clearInterval(this.timer);
+    }
+    //if totalTimer is false then run progresTimer() as to not reset totalTimer
+    if (!this.totalTimer) {
+      this.progressTimer();
     }
 
     this.timer = false;
@@ -49,6 +59,35 @@ export class RestTimerPage implements OnInit {
       this.percent = Math.floor((this.progress / totalSeconds) * 100);
       this.progress++;
     }, 1000)
+  }
+
+  progressTimer() {
+    let countDownDate = new Date();
+
+    this.totalTimer = setInterval(() => {
+      let now = new Date().getTime();
+
+      let distance = now - countDownDate.getTime();
+
+      // Time calculations for hours, minutes and seconds
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+
+    }, 1000)
+
+
+  }
+
+  pad(num, size) {
+    let s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
   }
 
 
