5e5f6667359f64275ad1a6570461ca6955c03d16
Levi Regan <S00108881@mail.itsligo.ie>
2020-02-18 13:54:49 +0000

    registration page: more fields added

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -5,27 +5,30 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+  styleUrls: ['./register.page.scss']
 })
 export class RegisterPage implements OnInit {
-
   newcredentialsForm: FormGroup;
- 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
-    ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
     this.newcredentialsForm = this.formBuilder.group({
       username: [''],
+      firstName: [''],
+      lastName: [''],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-    register() {
+  register() {
     this.authService.register(this.newcredentialsForm.value).subscribe(res => {
       // Call Login to automatically login the new user
       this.authService.login(this.newcredentialsForm.value).subscribe();
     });
   }
-
 }
