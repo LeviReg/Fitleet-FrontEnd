e85efd0f47724b4b61d3b594fd995c198406bdb5
Mark Walsh <S00181377@mail.itsligo.ie>
2020-03-03 14:13:07 +0000

    Api Key Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../app/comp/navbar/navbar.component';
+import { Environment } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   @Input() headerTitle: string;
@@ -30,9 +31,16 @@ export class AppComponent {
   CloseMenu() {
     this.menu.close();
   }
-  
+
   initializeApp() {
     this.platform.ready().then(() => {
+      Environment.setEnv({
+        // api key for server
+        API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyBpfdLQn2EEVNl8UgmZItNDxBBOqMGj8B0',
+
+        // api key for local development
+        API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyBpfdLQn2EEVNl8UgmZItNDxBBOqMGj8B0',
+      });
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
